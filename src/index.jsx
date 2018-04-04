c784a06871198cf63d4e3ba77b2ed387f2c1334b
Guillermo Casanova <guillermo@kodify.io>
2018-04-04 20:31:07 +0100

    Added linting - eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Navbar from './components/Navbar';
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
 
 // CUSTOM IMPORTS
 import 'bootstrap/dist/css/bootstrap.min.css';
